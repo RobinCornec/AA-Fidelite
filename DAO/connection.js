251d1db2feeff84978253f3541491caa1c86f158
RobinCornec <robin.cornec@epsi.fr>
2015-03-31 16:15:08 +0200

    v0.1.3
    Le serveur n'est plus jetable

--- DAO/connection.js
+++ DAO/connection.js
@@ -8,14 +8,12 @@ var connection = mysql.createConnection({
 });
 
 var selectuser = function (id, pwd, callback){  
-	connection.connect();
 
 	query = connection.query('SELECT * FROM users WHERE Username = "' + id + '"', function res(err, rows, field) {	
 
     callback(rows[0]);
     
   });
-	connection.end();
   
 };
 
