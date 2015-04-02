d29711ad6d2e2e9e85f6fe0618e84052effc65c4
RobinCornec <robin.cornec@epsi.fr>
2015-04-02 13:53:23 +0200

    v0.2.0
    WOUHOU LES POINTS DE FIDELITES CA MARCHE TROLOLOL UPDATE, SELECT, INSERT

--- DAO/connection.js
+++ DAO/connection.js
@@ -9,13 +9,53 @@ var connection = mysql.createConnection({
 
 var selectuser = function (login, pwd, callback){  
 
-	query = connection.query('SELECT * FROM users WHERE Username = "' + login + '"', function res(err, rows, field) {	
+	query = connection.query('SELECT * FROM users WHERE Username = "' + login + '";', function res(err, rows, field) {	
+    callback(rows[0]);  
+  });  
+};
 
-    callback(rows[0]);
-    
+var selecttemps = function (idVol, callback){  
+
+	query = connection.query('SELECT TempsMin FROM vols WHERE id = ' + idVol + ';', function res(err, rows, field) {	
+      callback(rows[0]);
+  	});
+  
+};
+
+var insertvol = function (iduser, idvol, date, callback){  
+
+	query = connection.query('INSERT INTO volpassagers (idVol, dateVol, idUser) VALUES (' + idvol + ', "' + date + '", ' + iduser + ');',
+	 function res(err) {	
+    	if (!err) {
+    		var msg = "insert vol is done !";
+    		callback(msg);
+    	}
+    	else{
+    		callback(err);
+    	};
+  });
+  
+};
+
+var updatetPointF = function (iduser, pf, callback){  
+
+	query = connection.query('UPDATE users SET pointFidelite = '+ pf +' WHERE id = "' + iduser + '";',
+	 function res(err) {	
+    	if (!err) {
+        var msg = "update pf is done !";
+    		callback(msg);
+    	}
+    	else{
+    		callback(err);
+    	};
   });
   
 };
 
 
+
+
 exports.selectuser = selectuser;
+exports.selecttemps = selecttemps;
+exports.insertvol = insertvol;
+exports.updatetPointF = updatetPointF;
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var app = express();
 var msg = "";
 var sess;
 
-app.use(session({secret: 'todotopsecret'}));
+app.use(session({secret: 'user'}));
 app.use(bodyParser.urlencoded({
   extended: true
 }));
@@ -17,38 +17,81 @@ app.use(bodyParser.json());
 
 app.get('/', function(req, res) { 
 	sess=req.session;
-	sess.test="vtff ca marche";
     res.render('index.ejs', {msg: msg});
 });
 
+app.get('/connect', function(req, res) { 
+	sess=req.session;
+	pwd = "";
+	if (sess.nom) {
+		reqmysql.selectuser(sess.login, pwd, function callback (result){	
+			sess.pf = result.pointFidelite;
+			res.render('mainPage.ejs', {nom: sess.nom, prenom: sess.prenom, pointFidelite: sess.pf});    		
+		});
+	}
+	else{
+		msg = "Veuillez vous connecter";
+		res.render('index.ejs', {msg: msg});
+		console.log(msg);
+	};
+    
+});
 
 app.post('/connect', function(req, res){
 	sess=req.session;
-	var login = req.body.identifiant;
-	var pwd = req.body.password;
-	reqmysql.selectuser(login, pwd, function callback (result){
-	sess=req.session;		
-		try{
+	if (req.body.identifiant) {
+		sess.login = req.body.identifiant;
+		var pwd = req.body.password;
+		reqmysql.selectuser(sess.login, pwd, function callback (result){	
+			try{
+				sess.nom = result.Nom;
+				sess.prenom = result.Prenom;
+				sess.pf = result.pointFidelite;
+				if (sess.login == result.Username && pwd == result.Password) {
+			        console.log(result.Prenom + " " + result.Nom + " s'est connecté");
+					res.render('mainPage.ejs', {nom: sess.nom, prenom: sess.prenom, pointFidelite: sess.pf});
+	    		}
+			 	else{
+		    		msg = "Mot de passe incorrect";
+		    		res.render('index.ejs', {msg: msg});
+		    		console.log(msg);
+	    		};
 
-			if (login == result.Username && pwd == result.Password) {
-		        console.log(result.Prenom + result.Nom + "s'est connecté");
-				res.render('mainPage.ejs', {nom: result.Nom, prenom: result.Prenom, test: sess.test});
-    		}
-		 	else{
-	    		msg = "Mot de passe incorrect";
-	    		res.render('index.ejs', {msg: msg});
-    		};
+			 }catch(msg){
+			 	msg = "Cette Username n'existe pas";
+				res.render('index.ejs', {msg: msg});
+			 	console.log(msg);
+			 } 	
+		});
+	}
+	else if (req.body.idPassager) {
+		var idPassager = req.body.idPassager;
+		var idVol = req.body.idVol;
+		var date = req.body.date;
+		var pfnew;
+		reqmysql.insertvol(idPassager, idVol, date, function callback (result){
+			console.log(result);
+		});
+		reqmysql.selecttemps(idVol, function callback (result){
+			pfnew = (sess.pf + result.TempsMin);
+			sess.pf = pfnew;
+			console.log(sess.prenom + " " + sess.nom + " a maintenant " + sess.pf + " points de fidélité");
+			reqmysql.updatetPointF(idPassager, pfnew, function callback (result){
+				console.log(result);
+				res.render('mainPage.ejs', {nom: sess.nom, prenom: sess.prenom, pointFidelite: sess.pf});   			
+			});
+		});
+		
+	}
+	else{
+		msg = "Veuillez vous connecté";
+		res.render('index.ejs', {msg: msg});
+		console.log(msg);
+	};
 
-		 }catch(msg){
-		 	var msg = "Cette Username n'existe pas";
-			res.render('index.ejs', {msg: msg});
-		 	console.log(msg);
-		 } 	
-	});
-	
 })
 
-.use(express.static(__dirname + '/')) ;
 
-app.listen(8080);
+.use(express.static(__dirname + '/'))
+.listen(8080);
 
